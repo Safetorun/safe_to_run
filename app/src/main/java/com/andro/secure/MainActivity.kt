45ec6fe6a32a2244a7945fb5fecf3c02cfb4ca6e
danielllewellyn <danieln.llewellyn@gmail.com>
2020-07-11 05:55:24 +0100

    Making a modular(ish) approach to how we can add capabilities

--- app/src/main/java/com/andro/secure/MainActivity.kt
+++ app/src/main/java/com/andro/secure/MainActivity.kt
@@ -2,10 +2,33 @@ package com.andro.secure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.andro.safetorun.SafeToRun
+import com.andro.safetorun.SafeToRunConfiguration
+import com.andro.safetorun.configure
+import com.andro.safetorun.features.blacklistedapps.BlacklistedAppCheck
+import com.andro.safetorun.features.rootdetection.rootDetection
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SafeToRun.init(
+            configure(this) {
+
+                // Root beer (detect root)
+                rootDetection {
+                    tolerateRoot = false
+                    tolerateBusyBox = true
+                }
+
+                // Black list certain apps
+
+            }
+        )
+
+        safeToRunResult.text = getString(R.string.safe_to_run_result, SafeToRun.isSafeToRun())
     }
 }
