88bdb06c700e631eca8303579834da0ddbeeb9e7
dllewellyn <danieln.llewellyn@gmail.com>
2021-05-23 10:46:48 +0100

    Adding a nice testing script..

--- scripts/test_local_sam_template.sh
+++ scripts/test_local_sam_template.sh
@@ -1,1 +1,3 @@
-./gradlew shadowJar && sam local invoke -e Resources/safeToRunDevice.json -t sam-template-testing.yml SafeToRunBackend
+./gradlew shadowJar &&
+sam local invoke -e Resources/safeToRunDevice.json -t sam-template-testing.yml --profile my_amplify_profile SafeToRunBackend > signature.json &&
+sam local invoke -e signature.json -t sam-template-testing.yml --profile my_amplify_profile SafeToRunBackendVerify
