993c5acfdc30d94418d24d2477c5527aa6cf6372
dllewellyn <danieln.llewellyn@gmail.com>
2021-05-28 17:03:07 +0100

    Adding KDoc comments

--- safeToRunCore/src/main/kotlin/io/github/dllewellyn/safetorun/features/blacklistedapps/BlacklistedAppConfiguration.kt
+++ safeToRunCore/src/main/kotlin/io/github/dllewellyn/safetorun/features/blacklistedapps/BlacklistedAppConfiguration.kt
@@ -2,19 +2,35 @@ package io.github.dllewellyn.safetorun.features.blacklistedapps
 
 import io.github.dllewellyn.safetorun.checks.SafeToRunCheck
 
-@DslMarker
-annotation class BlacklistedScope
-
-@BlacklistedScope
-class BlacklistedAppConfiguration(
+/**
+ * Configure applications which you want to avoid running on the same
+ * device as your app. Use [blacklistedAppConfiguration] to configure
+ */
+class BlacklistedAppConfiguration internal constructor(
     private val blacklistedAppCheck: BlacklistedAppCheck,
     private val appStrings: BlacklistedAppStrings
 ) {
 
     private val blacklistedApplications = mutableListOf<String>()
 
+    /**
+     * Add a blacklisted application
+     *
+     * @receiver the package name of the application to blacklist
+     */
     operator fun String.unaryPlus() = blacklistedApplications.add(this)
 
-    fun build(): SafeToRunCheck =
+    internal fun build(): SafeToRunCheck =
         BlacklistedApplicationDetection(blacklistedApplications, blacklistedAppCheck, appStrings)
+}
+
+fun blacklistedAppConfiguration(
+    blacklistedAppCheck: BlacklistedAppCheck,
+    appStrings: BlacklistedAppStrings,
+    block: BlacklistedAppConfiguration.() -> Unit
+): SafeToRunCheck {
+    return with(BlacklistedAppConfiguration(blacklistedAppCheck, appStrings)) {
+        block()
+        build()
+    }
 }
--- safeToRunCore/src/main/kotlin/io/github/dllewellyn/safetorun/features/installorigin/InstallOriginBuilder.kt
+++ safeToRunCore/src/main/kotlin/io/github/dllewellyn/safetorun/features/installorigin/InstallOriginBuilder.kt
@@ -2,21 +2,34 @@ package io.github.dllewellyn.safetorun.features.installorigin
 
 import io.github.dllewellyn.safetorun.checks.SafeToRunCheck
 
+/**
+ * Builder for an install origin check. Use [installOrigin] to get an instance
+ */
 class InstallOriginBuilder internal constructor(
     private val installOriginQuery: InstallOriginQuery,
     private val installOriginStrings: InstallOriginStrings
 ) {
     private val allowedOrigins = mutableListOf<InstallOrigin>()
 
+    /**
+     * Add a valid install origin
+     *
+     * @param installOrigin a valid install origin
+     */
     operator fun plus(installOrigin: InstallOrigin) {
         allowedOrigins.add(installOrigin)
     }
 
+    /**
+     * Add install origin using +
+     *
+     * @receiver Install origin to add
+     */
     operator fun InstallOrigin.unaryPlus() {
         plus(this)
     }
 
-    fun build(): SafeToRunCheck =
+    internal fun build(): SafeToRunCheck =
         InstallOriginCheck(installOriginStrings, allowedOrigins, installOriginQuery)
 }
 
