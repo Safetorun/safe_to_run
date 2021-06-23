f69ddc42a2a8b93afbbd1e243b64d6ed95b81f73
dllewellyn <danieln.llewellyn@gmail.com>
2021-06-23 18:07:55 +0100

    Adding intent verification & updating of some documentation

--- safeToRunIntentVerification/src/main/java/io/github/dllewellyn/safetorun/intents/IntentVerificationBuilder.kt
+++ safeToRunIntentVerification/src/main/java/io/github/dllewellyn/safetorun/intents/IntentVerificationBuilder.kt
@@ -2,9 +2,23 @@ package io.github.dllewellyn.safetorun.intents
 
 import android.content.Intent
 
+/**
+ * Class for building a verification builder use
+ *
+ * ```
+ * Intent.verify()
+ * ```
+ */
 class IntentVerificationBuilder internal constructor(private val intent: Intent) {
 
+    /**
+     * Whether to allow an 'intent' inside this bundle
+     */
     var allowContainingIntents: Boolean = false
+
+    /**
+     * Whether to allow any urls inside the bundle
+     */
     var allowAnyUrls: Boolean = false
 
     private val urlRegex by lazy {
@@ -14,20 +28,23 @@ class IntentVerificationBuilder internal constructor(private val intent: Intent)
     private var actionOnSuccess: (() -> Unit)? = null
     private var actionOnFailure: (() -> Unit)? = null
 
-    internal fun verify() = if (!allowContainingIntents) {
+    internal fun verify() =
+        (handleContainingIntents() && handleUrlStrings())
+            .also {
+                if (it) {
+                    actionOnSuccess?.invoke()
+                } else {
+                    actionOnFailure?.invoke()
+                }
+            }
+
+    private fun handleContainingIntents() = if (!allowContainingIntents) {
         intent.extras?.gatherAllIntents()?.isEmpty() == true
-                && urls()
     } else {
         true
-    }.also {
-        if (it) {
-            actionOnSuccess?.invoke()
-        } else {
-            actionOnFailure?.invoke()
-        }
     }
 
-    private fun urls() = if (!allowAnyUrls) {
+    private fun handleUrlStrings() = if (!allowAnyUrls) {
         intent.extras?.gatherAllStrings()?.any {
             urlRegex.matches(it)
         }?.not() ?: true
@@ -36,6 +53,11 @@ class IntentVerificationBuilder internal constructor(private val intent: Intent)
     }
 }
 
+/**
+ * Verify an intent based on the configuration
+ *
+ * @param builderBlock the configuration to use
+ */
 fun Intent.verify(builderBlock: IntentVerificationBuilder.() -> Unit) = IntentVerificationBuilder(this).run {
     builderBlock()
     verify()
