b4a983f01b7270187be388d07c02330916aeba46
dllewellyn <danieln.llewellyn@gmail.com>
2021-06-26 16:39:36 +0100

    Adding some documentation

--- app/src/main/java/com/andro/secure/intents/WebViewActivity.kt
+++ app/src/main/java/com/andro/secure/intents/WebViewActivity.kt
@@ -7,15 +7,35 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.andro.secure.R
 import com.andro.secure.databinding.ActivityWebViewBinding
+import io.github.dllewellyn.safetorun.intents.url.params.AllowedType
+import io.github.dllewellyn.safetorun.intents.url.urlVerification
 import io.github.dllewellyn.safetorun.intents.verify
+import java.lang.RuntimeException
 
 class WebViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Can do this specifically with the web verification tool
+
+        val url = intent?.extras?.getString("url")
+        val isUrlOk = url?.urlVerification {
+            "safetorun.com".allowHost()
+            allowParameter {
+                allowedType = AllowedType.String
+                parameterName = "a_token"
+            }
+        }
+
+        if (isUrlOk != true) {
+            throw RuntimeException("Failed validation!!")
+        }
+
         ActivityWebViewBinding.inflate(LayoutInflater.from(this)).apply {
             setContentView(root)
+
+            // Or do it with intent verification
 
             intent.verify {
 
