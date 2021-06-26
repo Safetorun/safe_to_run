c5eeb5e445bea7bf927795b1786ba90cb6c18a6f
dllewellyn <danieln.llewellyn@gmail.com>
2021-06-26 16:25:18 +0100

    Adding URL checks to project

--- safeToRunInputValidation/src/main/java/io/github/dllewellyn/safetorun/intents/url/UrlConfig.kt
+++ safeToRunInputValidation/src/main/java/io/github/dllewellyn/safetorun/intents/url/UrlConfig.kt
@@ -1,84 +1,68 @@
 package io.github.dllewellyn.safetorun.intents.url
 
-import android.net.Uri
 import io.github.dllewellyn.safetorun.intents.url.params.ParameterConfig
 import io.github.dllewellyn.safetorun.intents.url.params.ParameterConfigBuilder
-import io.github.dllewellyn.safetorun.intents.url.params.matchesAllowedType
-
-
-class UrlConfig {
-
-    private val allowedHost = mutableListOf<String>()
-    private val allowedUrls = mutableListOf<String>()
-    private val allowParameters = mutableListOf<ParameterConfig>()
-    private var allowAnyParameter = false
-    private var allowAnyUrl = false
-
-    fun String.allowHost() {
-        allowedHost.add(this)
-    }
-
-    fun String.allowUrl() {
-        allowedUrls.add(this)
-    }
-
-    fun allowParameter(parameter: ParameterConfig) {
-        this.allowParameters.add(parameter)
-    }
-
-    fun allowAnyParameter() {
-        allowAnyParameter = true
-    }
-
-    fun allowAnyUrl() {
-        allowAnyUrl = true
-    }
-
-    fun allowParameter(parameter: ParameterConfigBuilder.() -> Unit) {
-        ParameterConfigBuilder()
-            .apply(parameter)
-            .run { parameterName?.let { ParameterConfig(it, allowedType) } }
-            ?.also { allowParameters.add(it) }
-    }
-
-    internal fun verify(url: String): Boolean {
-        val uri = Uri.parse(url)
-
-        return allowAnyUrl || (url.urlsAreInAllowedLists()
-                && (
-                uri.queryParameterNames.isEmpty()
-                        || allowAnyParameter
-                        || uri.allParametersAccountFor()
-                ))
-    }
-
-    private fun Uri.allParametersAccountFor(): Boolean {
-        return queryParameterNames.map { actualParam ->
-            Pair(
-                actualParam,
-                allowParameters.firstOrNull { it.parameterName == actualParam })
-        }
-            .any {
-                it.second?.allowedType?.matchesAllowedType(getQueryParameter(it.first)) == true
-            }
-    }
-
-    private fun String.urlsAreInAllowedLists() =
-        allowedUrls.contains(this) ||
-                allowedHost.contains(hostNameMatcher.getHostName(this))
-
-}
 
 /**
- * Verify a URL matches the configuration
- *
- * @param config the configuration
- *
- * @receiver string url
- *
- * @return true if the check failed, false otherwise
+ * Url configuration
  */
-fun String.urlVerification(config: UrlConfig.() -> Unit) =
-    UrlConfig()
-        .apply(config)
-        .verify(this)
+interface UrlConfig {
+    /**
+     * Allow a host
+     *
+     * @receiver the host to allow
+     */
+    fun String.allowHost()
+
+    /**
+     * Allow a URL
+     *
+     * @receiver allow a url
+     */
+    fun String.allowUrl()
+
+    /**
+     * Allow a parameter
+     *
+     * @param parameter configuration
+     */
+    fun allowParameter(parameter: ParameterConfig)
+
+    /**
+     * Allow any parameters
+     *
+     * Warning - if possible, specify the parameters using `allowParameter(ParameterConfig)`
+     */
+    fun allowAnyParameter()
+
+    /**
+     * Allow any url
+     *
+     * Warning - if possible, specify a specific URL or a Host using `allowHost` and `allowUrl`
+     */
+    fun allowAnyUrl()
+
+    /**
+     * Allow a parameter
+     *
+     * Example:
+     *
+     * ```
+     * allowParameter {
+     *      parameterName = "queryparam"
+     *      allowedType = AllowedType.Boolean
+     * }
+     * ```
+     * @param parameter the configuration to use
+     */
+    fun allowParameter(parameter: ParameterConfigBuilder.() -> Unit)
+
+    /**
+     * Verify a URL
+     *
+     * @param url a url
+     *
+     * @return true if the URL is valid for use, false otherwise
+     */
+    fun verify(url: String): Boolean
+}
---
+++ safeToRunInputValidation/src/main/java/io/github/dllewellyn/safetorun/intents/url/UrlConfigImpl.kt
@@ -0,0 +1,83 @@
+package io.github.dllewellyn.safetorun.intents.url
+
+import android.net.Uri
+import io.github.dllewellyn.safetorun.intents.url.params.ParameterConfig
+import io.github.dllewellyn.safetorun.intents.url.params.ParameterConfigBuilder
+import io.github.dllewellyn.safetorun.intents.url.params.matchesAllowedType
+
+
+internal class UrlConfigImpl : UrlConfig {
+    private val allowedHost = mutableListOf<String>()
+    private val allowedUrls = mutableListOf<String>()
+    private val allowParameters = mutableListOf<ParameterConfig>()
+    private var allowAnyParameter = false
+    private var allowAnyUrl = false
+
+    override fun String.allowHost() {
+        allowedHost.add(this)
+    }
+
+    override fun String.allowUrl() {
+        allowedUrls.add(this)
+    }
+
+    override fun allowParameter(parameter: ParameterConfig) {
+        this.allowParameters.add(parameter)
+    }
+
+    override fun allowAnyParameter() {
+        allowAnyParameter = true
+    }
+
+    override fun allowAnyUrl() {
+        allowAnyUrl = true
+    }
+
+    override fun allowParameter(parameter: ParameterConfigBuilder.() -> Unit) {
+        ParameterConfigBuilder()
+            .apply(parameter)
+            .run { parameterName?.let { ParameterConfig(it, allowedType) } }
+            ?.also { allowParameters.add(it) }
+    }
+
+    override fun verify(url: String): Boolean {
+        val uri = Uri.parse(url)
+
+        return allowAnyUrl || (url.urlsAreInAllowedLists()
+                && (
+                uri.queryParameterNames.isEmpty()
+                        || allowAnyParameter
+                        || uri.allParametersAccountFor()
+                ))
+    }
+
+    private fun Uri.allParametersAccountFor(): Boolean {
+        return queryParameterNames.map { actualParam ->
+            Pair(
+                actualParam,
+                allowParameters.firstOrNull { it.parameterName == actualParam })
+        }
+            .any {
+                it.second?.allowedType?.matchesAllowedType(getQueryParameter(it.first)) == true
+            }
+    }
+
+    private fun String.urlsAreInAllowedLists() =
+        allowedUrls.contains(this) ||
+                allowedHost.contains(hostNameMatcher.getHostName(this))
+
+}
+
+/**
+ * Verify a URL matches the configuration
+ *
+ * @param config the configuration
+ *
+ * @receiver string url
+ *
+ * @return true if the check failed, false otherwise
+ */
+fun String.urlVerification(config: UrlConfig.() -> Unit) =
+    (UrlConfigImpl() as UrlConfig)
+        .apply(config)
+        .verify(this)
