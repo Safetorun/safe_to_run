d8e91bfcbe4e834ec9ea5c4a3dd83b5bc06ffa5f
dllewellyn <danieln.llewellyn@gmail.com>
2021-06-26 08:32:32 +0100

    Upgrading packages

--- safeToRunInputValidation/src/main/java/io/github/dllewellyn/safetorun/intents/url/HostNameMatcher.kt
+++ safeToRunInputValidation/src/main/java/io/github/dllewellyn/safetorun/intents/url/HostNameMatcher.kt
@@ -2,8 +2,22 @@ package io.github.dllewellyn.safetorun.intents.url
 
 import android.net.Uri
 
+/**
+ * Get the host name matcher
+ */
 fun interface HostNameMatcher {
+
+    /**
+     * Get the host name given a URL
+     *
+     * @param url the url to get a host for
+     *
+     * @return the host for this URL
+     */
     fun getHostName(url: String): String?
 }
 
+/**
+ * Host name matcher based on uri
+ */
 val hostNameMatcher = HostNameMatcher { Uri.parse(it).host }
