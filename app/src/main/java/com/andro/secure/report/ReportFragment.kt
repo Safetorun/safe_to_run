2f0128eecc4d5060287e09af0acaf7ec3a1a750d
dllewellyn <danieln.llewellyn@gmail.com>
2021-06-27 18:32:57 +0100

    Adding a sample of javascript interface vulnerabilities being thwarted by safe to run

---
+++ app/src/main/java/com/andro/secure/report/ReportFragment.kt
@@ -0,0 +1,187 @@
+package com.andro.secure.report
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.andro.secure.BuildConfig
+import com.andro.secure.ResultEpoxyController
+import com.andro.secure.databinding.ReportFragmentBinding
+import com.auth0.jwt.interfaces.DecodedJWT
+import io.github.dllewellyn.safetorun.SafeToRun
+import io.github.dllewellyn.safetorun.conditional.conditionalBuilder
+import io.github.dllewellyn.safetorun.configure
+import io.github.dllewellyn.safetorun.features.blacklistedapps.blacklistConfiguration
+import io.github.dllewellyn.safetorun.features.blacklistedapps.blacklistedAppCheck
+import io.github.dllewellyn.safetorun.features.blacklistedapps.rooting.blacklistRootingApps
+import io.github.dllewellyn.safetorun.features.debug.debugCheck
+import io.github.dllewellyn.safetorun.features.debug.isDebuggableCheck
+import io.github.dllewellyn.safetorun.features.installorigin.installOriginCheckWithDefaults
+import io.github.dllewellyn.safetorun.features.installorigin.installOriginCheckWithDefaultsCheck
+import io.github.dllewellyn.safetorun.features.oscheck.banAvdEmulator
+import io.github.dllewellyn.safetorun.features.oscheck.banBluestacksEmulator
+import io.github.dllewellyn.safetorun.features.oscheck.bannedBoardCheck
+import io.github.dllewellyn.safetorun.features.oscheck.bannedHardwareCheck
+import io.github.dllewellyn.safetorun.features.oscheck.bannedModel
+import io.github.dllewellyn.safetorun.features.oscheck.emulator.banAvdEmulatorCheck
+import io.github.dllewellyn.safetorun.features.oscheck.emulator.banBluestacksEmulatorCheck
+import io.github.dllewellyn.safetorun.features.oscheck.emulator.banGenymotionEmulatorCheck
+import io.github.dllewellyn.safetorun.features.oscheck.minOsVersion
+import io.github.dllewellyn.safetorun.features.oscheck.notManufacturer
+import io.github.dllewellyn.safetorun.features.oscheck.osDetectionCheck
+import io.github.dllewellyn.safetorun.features.oscheck.safeToRunCombinedCheck
+import io.github.dllewellyn.safetorun.features.rootdetection.rootDetection
+import io.github.dllewellyn.safetorun.features.rootdetection.rootDetectionCheck
+import io.github.dllewellyn.safetorun.features.signatureverify.verifySignatureCheck
+import io.github.dllewellyn.safetorun.features.signatureverify.verifySignatureConfig
+import io.github.dllewellyn.safetorun.inline.buildSafeToRunCheckList
+import io.github.dllewellyn.safetorun.inline.safeToRun
+import io.github.dllewellyn.safetorun.reporting.toGrouped
+import java.util.Date
+
+class ReportFragment : Fragment() {
+
+    private val reportsController = ResultEpoxyController()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        ReportFragmentBinding.inflate(inflater, container, false)
+            .apply { setup(this) }
+            .root
+
+    private fun setup(binding: ReportFragmentBinding) {
+        binding.runSensitiveAction.setOnClickListener {
+            requireContext().canIRun {
+                throw RuntimeException("Def")
+            }
+
+            Toast.makeText(it.context, "Performed sensitive action!!", Toast.LENGTH_LONG)
+                .show()
+
+        }
+
+        configureSafeToRunReporting()
+        binding.reportList.setController(reportsController)
+
+        requireContext().canIRun { Log.v("Failure", "Failure") }
+
+        reportsController.setData(SafeToRun.isSafeToRun().toGrouped(), mapOf())
+    }
+
+    private fun configureSafeToRunReporting() {
+        requireContext().apply {
+            SafeToRun.init(
+                configure {
+
+                    // Root beer (detect root)
+                    rootDetection {
+                        tolerateBusyBox = true
+                    }.error()
+
+                    // Black list certain apps
+                    blacklistConfiguration {
+                        +"com.abc.def"
+                        +packageName
+                        blacklistRootingApps()
+                    }.error()
+
+                    verifySignatureConfig("cSP1O3JN/8+Ag14WAOeOEnwAnpY=")
+                        .error()
+
+                    // OS Blacklist version
+                    osDetectionCheck(
+                        conditionalBuilder {
+                            with(minOsVersion(MIN_OS_VERSION))
+                            and(notManufacturer("Abc"))
+                            and(bannedModel("bannedModel"))
+                        }
+                    ).warn()
+
+                    osDetectionCheck(
+                        conditionalBuilder {
+                            with(bannedModel("Pixel 4a (5G)"))
+                        }
+                    ).warn()
+
+                    osDetectionCheck(banBluestacksEmulator()).error()
+
+                    installOriginCheckWithDefaults().warn()
+
+                    osDetectionCheck(banAvdEmulator()).error()
+                    debugCheck().warn()
+                }
+            )
+        }
+    }
+
+    private fun DecodedJWT.verifierResult(): Map<String, String> =
+        mapOf(
+            "correctIssuer" to (issuer == BuildConfig.API_KEY),
+            "anyFailures" to ((claims[Errors]?.asInt() ?: 1) > 0),
+            "expired" to expiresAt.after(Date()),
+            "anyWarnings" to ((claims[Warnings]?.asInt() ?: 1) > 0),
+            "anyPasses" to ((claims[Passes]?.asInt() ?: 1) > 0)
+        ).mapValues { it.value.toString() }
+
+
+    private inline fun Context.canIRun(actionOnFailure: () -> Unit) {
+        if (safeToRun(buildSafeToRunCheckList {
+                add {
+                    banAvdEmulatorCheck()
+                }
+
+                add {
+                    banGenymotionEmulatorCheck()
+                }
+
+                add {
+                    banBluestacksEmulatorCheck()
+                }
+
+                add {
+                    blacklistedAppCheck()
+                }
+
+                add {
+                    rootDetectionCheck()
+                }
+
+                add {
+                    isDebuggableCheck()
+                }
+
+                add {
+                    safeToRunCombinedCheck(
+                        listOf(
+                            { bannedHardwareCheck("hardware") },
+                            { bannedBoardCheck("board") }
+                        )
+                    )
+                }
+
+                add {
+                    safeToRunCombinedCheck(
+                        listOf { installOriginCheckWithDefaultsCheck() },
+                        listOf { !BuildConfig.DEBUG }
+                    )
+                }
+
+                add {
+                    verifySignatureCheck("Abc")
+                }
+
+            })()) {
+            actionOnFailure()
+        }
+    }
+
+    companion object {
+        const val MIN_OS_VERSION = 41
+        const val Warnings = "warnings"
+        const val Errors = "errors"
+        const val Passes = "passes"
+    }
+}
---
+++ app/src/main/java/com/andro/secure/webview/WebViewSample.kt
@@ -0,0 +1,48 @@
+package com.andro.secure.webview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.webkit.JavascriptInterface
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.andro.secure.databinding.WebViewFragmentBinding
+import com.andro.secure.util.logVerbose
+import io.github.dllewellyn.safetorun.intents.url.urlVerification
+
+class WebViewSample : Fragment() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        WebViewFragmentBinding.inflate(inflater, container, false)
+            .apply {
+                webViewSample.webViewClient = WebViewClientSample()
+                webViewSample.settings.javaScriptEnabled = true
+                webViewSample.addJavascriptInterface(this@WebViewSample, "app")
+            }
+            .apply { webViewSample.loadUrl("file:///android_asset/index.html") }
+            .root
+
+    @JavascriptInterface
+    fun executeFunction() {
+        logVerbose("Executed function. Imagine this was sensitive..")
+    }
+
+    class WebViewClientSample : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            val result = request?.url?.toString()?.urlVerification {
+                "file:///android_asset/index.html".allowUrl()
+            }
+
+            return if (result == true) {
+                super.shouldOverrideUrlLoading(view, request)
+            } else {
+                true
+            }
+        }
+    }
+}
