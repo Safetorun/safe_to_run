8d3754c600f4b1b1d7c3e8b104ba35b2cb2a06e6
dllewellyn <danieln.llewellyn@gmail.com>
2021-06-29 12:33:31 +0100

    Adding sitemap

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -51,15 +51,7 @@ module.exports = {
             additionalLanguages: ['kotlin', 'groovy'],
         }
     },
-    plugins: [
-        ['@docusaurus/plugin-sitemap',
-            {
-                changefreq: 'weekly',
-                priority: 0.5,
-                trailingSlash: false,
-            }
-        ]
-    ],
+
     presets: [
         [
             '@docusaurus/preset-classic',
@@ -71,5 +63,13 @@ module.exports = {
                 },
             },
         ],
+        [
+            '@docusaurus/plugin-sitemap',
+            {
+                changefreq: 'weekly',
+                priority: 0.5,
+                trailingSlash: false,
+            }
+        ]
     ],
 };
