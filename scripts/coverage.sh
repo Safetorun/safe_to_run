6ea8ab44775b6356581bd71cddab3562779ae20a
dllewellyn <danieln.llewellyn@gmail.com>
2021-09-25 15:04:39 +0100

    Removing the coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,4 @@
 ./gradlew spotlessApply detekt build test\
     :safeToRunCore:jacocoTestReport :safeToRun:jacocoTestReportDebug \
-    :safeToRun:generateJacocoBadge :safeToRunCore:generateJacocoBadge \
-    :safeToRunInternal:jacocoTestReport :safeToRunInternal:generateJacocoBadge
+    :safeToRun:generateJacocoBadge :safeToRunCore:generateJacocoBadge #\
+    #:safeToRunInternal:jacocoTestReport :safeToRunInternal:generateJacocoBadge
