3ac3ac4a00a9070e981dcd7dfd22487fa275720f
dllewellyn <danieln.llewellyn@gmail.com>
2021-10-15 18:20:05 +0100

    Downgrading min os version - no need to be so high

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
     someSidebar: {
         Overview: ['why'],
         Checks: ['checks/gettingstarted', 'checks/combinechecks','rootdetection', 'blacklisting', 'oscheck', 'emulatorcheck', 'debugcheck', 'signatureverification', 'installorigin'],
-        'Input verification' : ['input/gettingstarted.md', 'input/verifyurls', 'input/files', 'input/intentverification'],
+        'Input verification' : ['input/gettingstarted', 'input/verifyurls', 'input/files', 'input/intentverification'],
         Reporting: ['reporting_or_default', 'reporting/safetorunreporting']
     },
 };
