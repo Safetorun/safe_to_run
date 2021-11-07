f9d16aea0c9e531e836dc8aac313f6ce0e64cd60
dllewellyn <danieln.llewellyn@gmail.com>
2021-11-07 20:07:49 +0000

    Fix coverage.sh step to not really do coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,4 +1,1 @@
-./gradlew spotlessApply detekt build test\
-    :safeToRunCore:jacocoTestReport :safeToRun:jacocoTestReportDebug \
-    :safeToRun:generateJacocoBadge :safeToRunCore:generateJacocoBadge \
-    :safeToRunInputValidation:testReleaseUnitTestCoverage :safeToRunInputValidation:generateJacocoBadge
+./gradlew spotlessApply detekt build test
