ae9df6269939997879daae58ba24bd494936386c
dllewellyn <danieln.llewellyn@gmail.com>
2021-11-08 16:58:22 +0000

    Delete files that shouldn't be in there

--- safeToRunCore/bin/main/io/github/dllewellyn/safetorun/features/oscheck/OSDetectionConfig.kt
+++
@@ -1,5 +0,0 @@
-package io.github.dllewellyn.safetorun.features.oscheck
-
-import io.github.dllewellyn.safetorun.conditional.Conditional
-
-internal data class OSDetectionConfig(val bannedOsResult: List<Conditional>)
