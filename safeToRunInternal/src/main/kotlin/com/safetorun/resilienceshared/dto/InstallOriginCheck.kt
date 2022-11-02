e663503d9402fd7d04916fe3b25e09c6ab01a09c
dllewellyn <danieln.llewellyn@gmail.com>
2022-11-02 19:55:54 +0000

    Fixing codacy issue

--- safeToRunInternal/src/main/kotlin/com/safetorun/resilienceshared/dto/InstallOriginCheck.kt
+++ safeToRunInternal/src/main/kotlin/com/safetorun/resilienceshared/dto/InstallOriginCheck.kt
@@ -1,5 +1,8 @@
 package com.safetorun.resilienceshared.dto
 
+/**
+ * Adding missing comment
+ */
 @kotlinx.serialization.Serializable
 data class InstallOriginCheck(
     var allowedInstallOrigins: List<String> = emptyList()
