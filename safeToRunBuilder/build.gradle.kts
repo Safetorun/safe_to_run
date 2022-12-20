595ba1945eb3f9b348b863878a96452e7817a24a
dllewellyn <danieln.llewellyn@gmail.com>
2022-12-20 07:50:24 +0000

    Publishing alpha version of builder

--- safeToRunBuilder/build.gradle.kts
+++ safeToRunBuilder/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
     id("io.gitlab.arturbosch.detekt")
     id("com.diffplug.spotless")
+    id("org.jetbrains.dokka")
     id("com.github.johnrengelman.shadow") version "5.0.0"
     `java-gradle-plugin`
 }
