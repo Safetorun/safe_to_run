b32e474bbf3dd6723878243ff57eb792fed1c95b
dllewellyn <danieln.llewellyn@gmail.com>
2023-06-01 06:48:45 +0100

    Fixing bug in tests and increasing some coverage.

--- safeToRun/src/test/kotlin/com/safetorun/reporting/LoggerBackendSynchroniserTest.kt
+++ safeToRun/src/test/kotlin/com/safetorun/reporting/LoggerBackendSynchroniserTest.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import com.google.common.truth.Truth.assertThat
+import com.safetorun.logger.deleteLog
 import com.safetorun.logger.logs
 import com.safetorun.logger.models.AppMetadata
 import com.safetorun.logger.models.DeviceInformation
@@ -41,11 +42,12 @@ internal class LoggerBackendSynchroniserTest : TestCase() {
         }
 
         coEvery { context.logs() } coAnswers { returnList.asFlow() }
+        coEvery { context.deleteLog(any()) } coAnswers { /** Noop **/ }
     }
 
     fun `test that all items returned from logs are synchronised with the backend`() = runTest {
-
         val inputData = workDataOf(LoggerBackendSynchroniser.KEY_API_KEY to "Abc")
+        listAtEnd.clear()
 
         LoggerBackendSynchroniser(context, mockk<WorkerParameters>(relaxed = true).also {
             every { it.inputData } returns inputData
