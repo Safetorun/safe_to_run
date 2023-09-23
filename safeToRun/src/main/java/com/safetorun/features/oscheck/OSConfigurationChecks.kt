4fc68b8630e08f3f3610e542a6d9a915a9b77f78
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2023-09-23 10:08:23 +0100

    Revert "Revert "Various fixes to tests""
    This reverts commit e58f8fa87ffaa348a4adb0b901dd8f8c5a9b2d2c.

--- safeToRun/src/main/java/com/safetorun/features/oscheck/OSConfigurationChecks.kt
+++ safeToRun/src/main/java/com/safetorun/features/oscheck/OSConfigurationChecks.kt
@@ -7,8 +7,11 @@ import android.os.Build
  *
  * @param minOSVersion minimum os version
  */
-inline fun minOsVersionCheck(minOSVersion: Int) =
-    minOSVersion > Build.VERSION.SDK_INT
+inline fun minOsVersionCheck(
+    minOSVersion: Int,
+    osCheck: OSInformationQuery = osInformationQuery()
+) =
+    minOSVersion > osCheck.osVersion()
 
 /**
  * Configure to disallow a manufacturer - i.e. fail if it's this
@@ -16,8 +19,11 @@ inline fun minOsVersionCheck(minOSVersion: Int) =
  *
  * @param manufacturerName name of the manufacturer
  */
-inline fun notManufacturerCheck(manufacturerName: String) =
-    Build.MANUFACTURER == manufacturerName
+inline fun notManufacturerCheck(
+    manufacturerName: String,
+    osCheck: OSInformationQuery = osInformationQuery()
+) =
+    osCheck.manufacturer() == manufacturerName
 
 /**
  * Ban a model from running - i.e. if you add a model that
@@ -25,57 +31,65 @@ inline fun notManufacturerCheck(manufacturerName: String) =
  *
  * @param bannedModel the model to ban
  */
-inline fun bannedModelCheck(bannedModel: String) =
-    Build.MODEL == bannedModel
+inline fun bannedModelCheck(
+    bannedModel: String,
+    osCheck: OSInformationQuery = osInformationQuery()
+) =
+    osCheck.model() == bannedModel
 
 /**
  * Add a banned board to the list
  *
  * @param bannedBoard the model to ban
  */
-inline fun bannedBoardCheck(bannedBoard: String) =
-    Build.BOARD == bannedBoard
+inline fun bannedBoardCheck(
+    bannedBoard: String,
+    osCheck: OSInformationQuery = osInformationQuery()
+) =
+    osCheck.board() == bannedBoard
 
 /**
  * Add a banned cpuAbis
  *
  * @param cpuAbi the model to ban
  */
-inline fun bannedCpusCheck(cpuAbi: String) =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        Build.SUPPORTED_ABIS.contains(cpuAbi)
-    } else {
-        false
-    }
+inline fun bannedCpusCheck(cpuAbi: String, osCheck: OSInformationQuery = osInformationQuery()) =
+    osCheck.cpuAbi().contains(cpuAbi)
 
 /**
  * Add a banned bootloader to the list
  *
  * @param bannedBootloader the model to ban
  */
-inline fun bannedBootloaderCheck(bannedBootloader: String) =
-    Build.BOOTLOADER == bannedBootloader
+inline fun bannedBootloaderCheck(
+    bannedBootloader: String,
+    osCheck: OSInformationQuery = osInformationQuery()
+) =
+    osCheck.bootloader() == bannedBootloader
 
 /**
  * Add a banned device
  *
  * @param device device to ban
  */
-inline fun bannedDeviceCheck(device: String) =
-    Build.DEVICE == device
+inline fun bannedDeviceCheck(device: String, osCheck: OSInformationQuery = osInformationQuery()) =
+    osCheck.device() == device
 
 /**
  * Add a banned host
  *
  * @param hardware hardware to ban
  */
-inline fun bannedHardwareCheck(hardware: String) =
-    Build.HARDWARE == hardware
+inline fun bannedHardwareCheck(
+    hardware: String,
+    osCheck: OSInformationQuery = osInformationQuery()
+) =
+    osCheck.hardware() == hardware
 
 /**
  * Add a banned host
  *
  * @param host the model to ban
  */
-inline fun bannedHostCheck(host: String) =
-    Build.HOST == host
+inline fun bannedHostCheck(host: String, osCheck: OSInformationQuery = osInformationQuery()) =
+    osCheck.host() == host
--- safeToRun/src/test/kotlin/com/safetorun/features/oscheck/emulator/EmulatorsKtTest.kt
+++ safeToRun/src/test/kotlin/com/safetorun/features/oscheck/emulator/EmulatorsKtTest.kt
@@ -1,116 +1,122 @@
 package com.safetorun.features.oscheck.emulator
 
-import android.os.Build
 import com.google.common.truth.Truth.assertThat
+import com.safetorun.features.oscheck.OSInformationQuery
 import com.safetorun.features.oscheck.OsCheckConstants
-import com.safetorun.utils.mockBuildField
+import com.safetorun.features.oscheck.OsCheckConstants.GENYMOTION_MANUFACTURER
+import io.mockk.every
+import io.mockk.mockk
 import junit.framework.TestCase
 
 internal class EmulatorsKtTest : TestCase() {
+    private val osCheck by lazy { mockk<OSInformationQuery>() }
 
     override fun setUp() {
         super.setUp()
-        mockBuildField(NOT_BANNED, "MANUFACTURER", Build::class.java)
+        every { osCheck.manufacturer() } returns NOT_BANNED
     }
 
     fun `test that avd emulator check fails if bootloader is unknown`() {
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "DEVICE", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
-        assertThat(banAvdEmulatorCheck()).isTrue()
+        every { osCheck.bootloader() } returns OsCheckConstants.UNKNOWN
+        every { osCheck.device() } returns OsCheckConstants.AVD_DEVICE_TYPE
+        every { osCheck.board() } returns NOT_BANNED
+
+
+        assertThat(banAvdEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that avd emulator check fails if device is banned`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        mockBuildField(OsCheckConstants.AVD_DEVICE_TYPE, "DEVICE", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
+        every { osCheck.bootloader() } returns NOT_BANNED
+        every { osCheck.device() } returns OsCheckConstants.AVD_DEVICE_TYPE
+        every { osCheck.board() } returns NOT_BANNED
 
-        assertThat(banAvdEmulatorCheck()).isTrue()
+        assertThat(banAvdEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that avd emulator check fails if board is banned`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "DEVICE", Build::class.java)
-        mockBuildField(OsCheckConstants.AVD_EMULATOR_BOARD, "BOARD", Build::class.java)
+        every { osCheck.bootloader() } returns NOT_BANNED
+        every { osCheck.device() } returns NOT_BANNED
+        every { osCheck.board() } returns OsCheckConstants.AVD_EMULATOR_BOARD
 
-        assertThat(banAvdEmulatorCheck()).isTrue()
+        assertThat(banAvdEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that avd emulator check passes if it passes`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "DEVICE", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
-        assertThat(banAvdEmulatorCheck()).isFalse()
+        every { osCheck.bootloader() } returns NOT_BANNED
+        every { osCheck.device() } returns NOT_BANNED
+        every { osCheck.board() } returns NOT_BANNED
+
+        assertThat(banAvdEmulatorCheck(osCheck)).isFalse()
     }
 
     fun `test that avd bluestacks check fails if it fails`() {
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOOTLOADER", Build::class.java)
-        assertThat(banBluestacksEmulatorCheck()).isTrue()
+        every { osCheck.bootloader() } returns OsCheckConstants.UNKNOWN
+        assertThat(banBluestacksEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that bluestacks emulator check passes if it passes`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        assertThat(banBluestacksEmulatorCheck()).isFalse()
+        every { osCheck.bootloader() } returns NOT_BANNED
+        assertThat(banBluestacksEmulatorCheck(osCheck)).isFalse()
     }
 
     fun `test that genymotion emulator check fails if bootloader check fails`() {
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "MANUFACTURER", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
+        every { osCheck.bootloader() } returns OsCheckConstants.UNKNOWN
+        every { osCheck.manufacturer() } returns NOT_BANNED
+        every { osCheck.board() } returns NOT_BANNED
 
-        assertThat(banGenymotionEmulatorCheck()).isTrue()
+
+        assertThat(banGenymotionEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that genymotion emulator check fails if manufacturer check fails`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        mockBuildField(OsCheckConstants.GENYMOTION_MANUFACTURER, "MANUFACTURER", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
 
-        assertThat(banGenymotionEmulatorCheck()).isTrue()
+        every { osCheck.bootloader() } returns NOT_BANNED
+        every { osCheck.manufacturer() } returns GENYMOTION_MANUFACTURER
+        every { osCheck.board() } returns NOT_BANNED
+
+        assertThat(banGenymotionEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that genymotion emulator check fails if board check fails`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "MANUFACTURER", Build::class.java)
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOARD", Build::class.java)
+        every { osCheck.bootloader() } returns NOT_BANNED
+        every { osCheck.manufacturer() } returns NOT_BANNED
+        every { osCheck.board() } returns OsCheckConstants.UNKNOWN
 
-        assertThat(banGenymotionEmulatorCheck()).isTrue()
+        assertThat(banGenymotionEmulatorCheck(osCheck)).isTrue()
     }
 
     fun `test that genymotion emulator check passes if it passes`() {
-        mockBuildField(NOT_BANNED, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "MANUFACTURER", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
+        every { osCheck.bootloader() } returns NOT_BANNED
+        every { osCheck.manufacturer() } returns NOT_BANNED
+        every { osCheck.board() } returns NOT_BANNED
 
-        assertThat(banGenymotionEmulatorCheck()).isFalse()
+        assertThat(banGenymotionEmulatorCheck(osCheck)).isFalse()
     }
 
     // Xiaomi checks
     fun `test that avd emulator check passes if bootloader is unknown but device is Xiaomi`() {
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "DEVICE", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
-        mockBuildField(OsCheckConstants.XIAOMI, "MANUFACTURER", Build::class.java)
+        every { osCheck.bootloader() } returns OsCheckConstants.UNKNOWN
+        every { osCheck.manufacturer() } returns OsCheckConstants.XIAOMI
+        every { osCheck.board() } returns NOT_BANNED
+        every { osCheck.device() } returns NOT_BANNED
 
-        assertThat(banAvdEmulatorCheck()).isFalse()
+        assertThat(banAvdEmulatorCheck(osCheck)).isFalse()
     }
 
 
     fun `test that genymotion emulator check passes if it passes but device is Xiaomi`() {
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOOTLOADER", Build::class.java)
-        mockBuildField(NOT_BANNED, "MANUFACTURER", Build::class.java)
-        mockBuildField(NOT_BANNED, "BOARD", Build::class.java)
+        every { osCheck.bootloader() } returns OsCheckConstants.UNKNOWN
+        every { osCheck.manufacturer() } returns OsCheckConstants.XIAOMI
+        every { osCheck.board() } returns NOT_BANNED
+        every { osCheck.device() } returns NOT_BANNED
 
-        mockBuildField(OsCheckConstants.XIAOMI, "MANUFACTURER", Build::class.java)
-
-        assertThat(banGenymotionEmulatorCheck()).isFalse()
+        assertThat(banGenymotionEmulatorCheck(osCheck)).isFalse()
     }
 
     fun `test that avd bluestacks check passes if it fails but device is Xiaomi`() {
-        mockBuildField(OsCheckConstants.UNKNOWN, "BOOTLOADER", Build::class.java)
-
-        mockBuildField(OsCheckConstants.XIAOMI, "MANUFACTURER", Build::class.java)
-        assertThat(banBluestacksEmulatorCheck()).isFalse()
+        every { osCheck.bootloader() } returns OsCheckConstants.UNKNOWN
+        every { osCheck.manufacturer() } returns OsCheckConstants.XIAOMI
+        assertThat(banBluestacksEmulatorCheck(osCheck)).isFalse()
     }
 
 
