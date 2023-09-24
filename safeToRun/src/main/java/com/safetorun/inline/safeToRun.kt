77ec689b5dc8d47e1a72c123baae3c8117a64d66
dllewellyn <danieln.llewellyn@gmail.com>
2023-09-24 08:42:46 +0100

    Removing plus.

--- safeToRun/src/main/java/com/safetorun/inline/safeToRun.kt
+++ safeToRun/src/main/java/com/safetorun/inline/safeToRun.kt
@@ -1,57 +1,53 @@
 package com.safetorun.inline
 
-import android.content.Context
-import com.safetorun.plus.logger as plusLogger
-import com.safetorun.plus.verifyLogger as verify
-
 /**
  * Safe to run check (Return true if the check fails)
  */
 typealias SafeToRunCheck = () -> Boolean
-
-/**
- * Initialise safe to run plus
- *
- * @param apiKey api key for the backend
- */
-fun initialiseSafeToRunPlus(apiKey: String) {
-    com.safetorun.plus.initialiseSafeToRunPlus(apiKey)
-}
-
-/**
- * Safe to run logger for verify.
- *
- * Note: Be sure you call [initialiseSafeToRunPlus] before using this method
- */
-fun <T> Context.verifyLogger(
-    checkName: String
-): (Boolean, T?) -> Unit = { value, extraData ->
-    verify<T>(checkName).invoke(value, extraData)
-}
-
-/**
- * Safe to run logger for verify.
- *
- * Note: Be sure you call [initialiseSafeToRunPlus] before using this method
- */
-fun Context.logger(checkName: String): (Boolean) -> Unit = {
-    plusLogger(checkName).invoke(it)
-}
-
-/**
- * Safe to run logger for checker
- */
-@Deprecated(
-    "Use initialiseSafeToRunPlus and logger instead",
-    ReplaceWith(
-        "initialiseSafeToRunPlus(apiKey); logger(checkName)",
-        imports = ["com.safetorun.plus.initialiseSafeToRunPlus", "com.safetorun.plus.logger"]
-    )
-)
-fun Context.logger(apiKey: String, checkName: String): (Boolean) -> Unit = {
-    initialiseSafeToRunPlus(apiKey)
-    plusLogger(checkName).invoke(it)
-}
+//
+///**
+// * Initialise safe to run plus
+// *
+// * @param apiKey api key for the backend
+// */
+//fun initialiseSafeToRunPlus(apiKey: String) {
+//    com.safetorun.plus.initialiseSafeToRunPlus(apiKey)
+//}
+//
+///**
+// * Safe to run logger for verify.
+// *
+// * Note: Be sure you call [initialiseSafeToRunPlus] before using this method
+// */
+//fun <T> Context.verifyLogger(
+//    checkName: String
+//): (Boolean, T?) -> Unit = { value, extraData ->
+//    verify<T>(checkName).invoke(value, extraData)
+//}
+//
+///**
+// * Safe to run logger for verify.
+// *
+// * Note: Be sure you call [initialiseSafeToRunPlus] before using this method
+// */
+//fun Context.logger(checkName: String): (Boolean) -> Unit = {
+//    plusLogger(checkName).invoke(it)
+//}
+//
+///**
+// * Safe to run logger for checker
+// */
+//@Deprecated(
+//    "Use initialiseSafeToRunPlus and logger instead",
+//    ReplaceWith(
+//        "initialiseSafeToRunPlus(apiKey); logger(checkName)",
+//        imports = ["com.safetorun.plus.initialiseSafeToRunPlus", "com.safetorun.plus.logger"]
+//    )
+//)
+//fun Context.logger(apiKey: String, checkName: String): (Boolean) -> Unit = {
+//    initialiseSafeToRunPlus(apiKey)
+//    plusLogger(checkName).invoke(it)
+//}
 
 /**
  * Configure
