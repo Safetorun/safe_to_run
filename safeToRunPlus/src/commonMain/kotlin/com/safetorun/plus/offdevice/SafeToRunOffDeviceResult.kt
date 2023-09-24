2da601330a6724cc8ffc87523686c51f37417544
dllewellyn <danieln.llewellyn@gmail.com>
2023-09-24 07:57:14 +0100

    Removing plus.

--- safeToRunPlus/src/commonMain/kotlin/com/safetorun/plus/offdevice/SafeToRunOffDeviceResult.kt
+++
@@ -1,8 +0,0 @@
-package com.safetorun.plus.offdevice
-
-/**
- * The result of a safe to run check that was run off device
- *
- * @param signedResult the signed result (a JWT)
- */
-data class SafeToRunOffDeviceResult(val signedResult: String)
--- safeToRunPlus/src/commonMain/kotlin/com/safetorun/plus/repository/DeviceIdRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.safetorun.plus.repository
-
-internal interface DeviceIdRepository {
-    fun getOrCreateDeviceIdSync(): String
-    fun getOrCreateDeviceIdAsync(deviceId: (String) -> Unit)
-}
